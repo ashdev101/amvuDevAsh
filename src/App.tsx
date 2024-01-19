45c576561bc091b4e77b745e242a81f9c1e63aa6
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-01-19 18:27:03 +0530

    uuhu

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,27 @@ import About from "./pages/about";
 import CampusAmbassador from "./pages/campus-ambassador";
 import Home from "./pages/home";
 import ServicesPage from "./pages/services";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [scrollPercentage, setScrollPercentage] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const currentPosition = window.scrollY;
+      const percentage = (currentPosition / scrollHeight) * 100;
+      setScrollPercentage(percentage);
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [])
   return (
     <>
       <BrowserRouter>
+        <div className="progress-bar" style={{ width: `${scrollPercentage}%` }} />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
