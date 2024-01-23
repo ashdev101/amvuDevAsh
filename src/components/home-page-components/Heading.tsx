0afc51b48b70bb9abe5b993ba18e5d8343c5110c
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-01-23 21:35:25 +0530

    ferjhb

--- src/components/home-page-components/Heading.tsx
+++ src/components/home-page-components/Heading.tsx
@@ -1,4 +1,4 @@
-import { Animator, MoveOut, ScrollContainer } from "react-scroll-motion";
+import { Animator, FadeOut, MoveOut, ScrollContainer, ZoomIn, ZoomOut } from "react-scroll-motion";
 import "../../styles/home-styles/heading.css";
 import { socialMediaLinks } from "../../utils/home-utils/homeUtils";
 import { AmbuviansImage, IllusBG } from "../../utils/home-utils/images";
@@ -9,11 +9,10 @@ const Heading = () => {
   return (
     <div className="flex relative xl:h-[700px] lg:h-[600px] md:h-[500px] sm:h-[380px] h-[320px]">
       {/* Heading and paragraph div - Left Side Content */}
-      <div className="w-[50%] flex items-start flex-col relative">
-        <div>
-          <ScrollContainer>
-            <Animator animation={MoveOut(0,-400)}>
-
+      <ScrollContainer>
+        <Animator animation={MoveOut(0, -400)}>
+          <div className="w-[50%] flex items-start flex-col relative">
+            <div className="animate-in">
 
               <h1 className="heading-H1">
                 Ambuvians <br />
@@ -28,32 +27,38 @@ const Heading = () => {
                 skilled medical professionals and advanced technologies."
               </p>
 
-            </Animator>
-
-          </ScrollContainer>
-        </div>
-        {/* Social Links Div */}
-        <div
-          className="heading-social-links-div">
-          {socialMediaLinks &&
-            socialMediaLinks.map((sociallink, index) => (
-              <a
-                key={index}
-                href={sociallink.href}
-                className="heading-social-links-a btnFillup liquidbg"
-              >
-                <sociallink.icon className="heading-social-link-icons" />
-              </a>
-            ))}
-          <a href="" className="btn">Explore</a>
-        </div>
-      </div>
-
+            </div>
+            {/* Social Links Div */}
+            <div
+              className="heading-social-links-div animate-in">
+              {socialMediaLinks &&
+                socialMediaLinks.map((sociallink, index) => (
+                  <a
+                    key={index}
+                    href={sociallink.href}
+                    className="heading-social-links-a btnFillup liquidbg"
+                  >
+                    <sociallink.icon className="heading-social-link-icons" />
+                  </a>
+                ))}
+              <a href="" className="btn">Explore</a>
+            </div>
+          </div>
+        </Animator>
+      </ScrollContainer>
       {/* Ambuvians Image Div - Right Side Content */}
       <div>
-        <img src={IllusBG} alt="" className="rotate 2xl:w-[619px] xl:w-[550px] lg:w-[430px] xl:top-[20px] lg:top-[30px] absolute lg:flex hidden top-0 2xl:left-[760px] xl:left-[650px] lg:left-[580px] -z-0" />
+        <ScrollContainer>
+          <Animator animation={FadeOut(1, -1)}>
+            <img src={IllusBG} alt="" className="rotate 2xl:w-[619px] xl:w-[550px] lg:w-[430px] xl:top-[20px] lg:top-[30px] absolute lg:flex hidden top-0 2xl:left-[760px] xl:left-[650px] lg:left-[580px] -z-0" />
+          </Animator>
+        </ScrollContainer>
         <div className="heading-ambuvians-image-div">
-          <img src={AmbuviansImage} alt="Ambuvians Image" className="heading-ambuvians-image" />
+          <ScrollContainer>
+            <Animator animation={ZoomOut(1, 0)}>
+              <img src={AmbuviansImage} alt="Ambuvians Image" className="heading-ambuvians-image animate-bounce-in" />
+            </Animator>
+          </ScrollContainer>
         </div>
       </div>
     </div>
