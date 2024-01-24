2cfdafc89d78d5748cb7fdb23633df225e1024cf
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-01-24 18:04:34 +0530

    anim

--- src/components/home-page-components/components/H2.tsx
+++ src/components/home-page-components/components/H2.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import "../../../styles/home-styles/home.css"
+import { useInView } from 'react-intersection-observer'
+import clsx from 'clsx'
 interface H2PropsTypes {
     text: string
 }
 const H2: React.FC<H2PropsTypes> = ({ text }) => {
+    const { ref, inView } = useInView()
     return (
-        <h2
-            className="all-home-components-heading-h1"
+        <h2 ref={ref}
+            className={clsx("all-home-components-heading-h1", inView && "h1-in")}
         >{text}
         </h2>
     )
