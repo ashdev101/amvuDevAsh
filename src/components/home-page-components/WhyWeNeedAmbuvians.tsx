644008deb9daec484cb5b373334aa077cb9d3f60
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-01-24 20:58:34 +0530

    efjn

--- src/components/home-page-components/WhyWeNeedAmbuvians.tsx
+++ src/components/home-page-components/WhyWeNeedAmbuvians.tsx
@@ -1,20 +1,25 @@
 import { Patient1, Patient2, PatientOnBike, Traffic, Vactor3, Vactor4 } from "../../utils/home-utils/images"
 import "../../styles/home-styles/whyWeNeedAmbuvians.css";
+import { useInView } from "react-intersection-observer";
+import H2 from "./components/H2";
+import clsx from "clsx";
 
 const WhyWeNeedAmbuvians = () => {
+    const { ref, inView } = useInView()
+    const { ref: imgRef, inView: imgInView } = useInView()
     return (
-        <div className="w-full relative lg:mt-[100px] sm:mt-[90px] mt-[60px]">
-            <h2 className="all-home-components-heading-h1">Why We Need Ambuvians</h2>
+        <div className="w-full relative lg:mt-[100px] sm:mt-[90px] mt-[60px]" ref={ref}>
+            <H2 text="Why We Need Ambuvians" />
 
             <div className="flex flex-col xl:mt-[120px] mt-[10px] xl:h-full h-[700px] relative">
 
                 {/* top content div */}
                 <div className="flex xl:flex-row flex-col-reverse justify-evenly">
-                    <div className="h-full flex justify-between items-baseline 2xl:w-[38%] xl:w-[44%] w-[38%]">
+                    <div className={clsx("h-full flex justify-between items-baseline 2xl:w-[38%] xl:w-[44%] w-[38%]", imgInView ? "image-in-view" : "animate-fadeout")} ref={imgRef}>
                         <img src={Traffic} alt="traffic" className="whyWeNeedAmbuvians-traffic-image" />
                         <img src={Patient1} alt="patient" className="whyWeNeedAmbuvians-patient1-image" />
                     </div>
-                    <div className="lg:w-[610px] h-[300px] ">
+                    <div className={clsx("lg:w-[610px] h-[300px]", inView ? "animate-fadein" : "animate-fadeout")}>
                         <h3 className="lg:w-full md:w-[50%] py-[20px] w-[70%] md:h-[128px] font-[700] text-[#DC1F27] xl:ml-0 sm:ml-[10%] ml-[4%] xl:text-[39px] md:text-[30px] text-[22px] md:leading-[55px] leading-[30px]">
                             Ambuvians is on a mission to address these problems
                         </h3>
@@ -25,7 +30,7 @@ const WhyWeNeedAmbuvians = () => {
                 </div>
 
                 {/* bottom content div */}
-                <div className="flex justify-evenly mt-[50px] 2xl:w-[80%] xl:w-[90%] w-[80%] m-auto">
+                <div className={clsx("flex justify-evenly mt-[50px] 2xl:w-[80%] xl:w-[90%] w-[80%] m-auto", imgInView ? "image-in-view" : "animate-fadeout")}>
                     <img src={PatientOnBike} alt="patient" className="whyWeNeedAmbuvians-patientOnBike-image" />
                     <img src={Patient2} alt="patient" className="whyWeNeedAmbuvians-patient2-image" />
                 </div>
@@ -33,8 +38,8 @@ const WhyWeNeedAmbuvians = () => {
 
             {/* Vactors */}
             <div>
-                <img src={Vactor3} className="whyWeNeedAmbuvians-vactor3" />
-                <img src={Vactor4} className="whyWeNeedAmbuvians-vactor4" />
+                <img src={Vactor3} className={clsx("whyWeNeedAmbuvians-vactor3", inView ? "animate-vactor-one" : "animate-fadeout")} />
+                <img src={Vactor4} className={clsx("whyWeNeedAmbuvians-vactor4", inView ? "animate-vactor-two" : "animate-fadeout")} />
             </div>
 
         </div>
