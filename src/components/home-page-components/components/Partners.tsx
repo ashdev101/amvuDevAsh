0d69cc6ebbdb3ffaf73561b59d3b0775e32d1bad
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-01-25 10:03:07 +0530

    fr

--- src/components/home-page-components/components/Partners.tsx
+++ src/components/home-page-components/components/Partners.tsx
@@ -1,10 +1,14 @@
+import { useInView } from "react-intersection-observer"
 import { ourPartners } from "../../../utils/home-utils/homeUtils"
+import H2 from "./H2"
+import clsx from "clsx"
 
 const Partners = () => {
+    const { ref, inView } = useInView()
     return (
         <div className="mt-[50px] flex flex-col">
-            <h1 className="all-home-components-heading-h1">Our Partners</h1>
-            <div className="lg:bg-[#FBFBFB] bg-slate-200 w-[90%] md:h-[198px] h-[130px] overflow-auto hideScrollbar gap-[20px] m-auto md:mt-[50px] mt-[30px] flex justify-between px-[70px] items-center z-10" style={{ borderRadius: "10px 52px 10px 42px" }} >
+            <H2 text="Our Partners" />
+            <div className={clsx("lg:bg-[#FBFBFB] bg-slate-200 w-[90%] md:h-[198px] h-[130px] overflow-auto hideScrollbar gap-[20px] m-auto md:mt-[50px] mt-[30px] flex justify-between px-[70px] items-center z-10", inView ? "animate-fadein" : "animate-fadeout")} style={{ borderRadius: "10px 52px 10px 42px" }} ref={ref} >
                 {
                     ourPartners.length && ourPartners.map((element, index) => (
                         <img src={element.icon} alt={element.alt} key={index} className="lg:w-[178px] md:w-[150px] sm:w-[100px] w-[70px] lg:h-[172px] md:h-[150px] sm:h-[100px] h-[70px] auto-scroll" />
