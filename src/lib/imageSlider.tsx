0edc4e830976f41b3bbcc5d499f8269b933a11fd
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-01-25 14:06:34 +0530

    kffnfjn

--- src/lib/imageSlider.tsx
+++ src/lib/imageSlider.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import "../index.css";
+import { useInView } from 'react-intersection-observer';
+import clsx from 'clsx';
 const ImageSlider = ({ images }: { images: string[] }) => {
+    const { ref, inView } = useInView()
     const [isVisible, setIsVisible] = useState(false)
     const [direction, setDirection] = useState("slider-image-right")
     const visibilityHandler = () => {
@@ -15,11 +18,11 @@ const ImageSlider = ({ images }: { images: string[] }) => {
         visibilityHandler()
         setDirection("slider-image-right")
     };
- /*    const prevSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-        visibilityHandler()
-        setDirection("slider-image-left")
-    }; */
+    /*    const prevSlide = () => {
+           setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+           visibilityHandler()
+           setDirection("slider-image-left")
+       }; */
 
     useEffect(() => {
         visibilityHandler()
@@ -30,10 +33,12 @@ const ImageSlider = ({ images }: { images: string[] }) => {
         return () => clearInterval(intervelId)
     }, [currentIndex])
 
+
+
     return (
         <div className="w-[100%] h-full m-auto flex hideScrollbar" style={{ display: isVisible ? "flex" : "none" }}>
             {/*             <button onClick={prevSlide} className='slider-btn'><FcPrevious /> </button> */}
-            <div className='w-full flex justify-center items-center'>
+            <div className={clsx('w-full flex justify-center items-center', inView ? "zoom-in" : "animate-fadeout")} ref={ref}>
                 <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className={direction} />
             </div>
             {/*         <button onClick={nextSlide} className='slider-btn'><FcNext /></button> */}
