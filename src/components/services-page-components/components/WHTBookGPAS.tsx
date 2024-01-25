9386ffa64cdf36e0757b2db0f3b1fb4112fe8fca
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-01-25 13:07:36 +0530

    sdnjb

--- src/components/services-page-components/components/WHTBookGPAS.tsx
+++ src/components/services-page-components/components/WHTBookGPAS.tsx
@@ -1,12 +1,16 @@
+import { useInView } from "react-intersection-observer";
 import "../../../styles/services-page-styles/WHTBookGPAS.css";
 import { AppStore, GooglePlay } from "../../../utils/footerUtils";
+import clsx from "clsx";
 
 const WHTBookGPAS = () => {
+    const { ref: rightRef, inView: rightInView } = useInView()
+    const { ref: leftRef, inView: leftInView } = useInView()
     return (
         <div className="WHTBook-GPAS-main-container">
 
             {/* Right Side Box */}
-            <div className="WHTBook-right-side-box">
+            <div className={clsx("WHTBook-right-side-box", rightInView ? "zoom-in" : "animate-fadeout")} ref={rightRef}>
                 <h3 className="WHTBook-right-side-box-h3">Schedule your ambulance ride right away because at Ambuvians, we prioritize saving lives.</h3>
                 <div className="WHTBook-box">
                     <p className="WHTBook-box-p">Watch How To Book</p>
@@ -15,7 +19,7 @@ const WHTBookGPAS = () => {
             </div>
 
             {/* Left Side Box */}
-            <div className="WHTBook-left-side-box">
+            <div className={clsx("WHTBook-left-side-box", leftInView ? "zoom-in" : "animate-fadeout")} ref={leftRef} >
                 <div className=" flex justify-center items-center w-full h-[50%] xl:gap-[30px] md:gap-[10px] gap-[5%]">
                     <img src={GooglePlay} alt="Google Play" className="WHTBook-google-play-app-store-image" />
                     <img src={AppStore} alt="App Store" className="WHTBook-google-play-app-store-image" />
