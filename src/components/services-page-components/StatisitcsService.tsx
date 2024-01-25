e1e84ba3685cb397f888a33b4167168f3d525c5c
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-01-25 13:55:58 +0530

    enjfb

--- src/components/services-page-components/StatisitcsService.tsx
+++ src/components/services-page-components/StatisitcsService.tsx
@@ -1,13 +1,8 @@
 import ImageSlider from "../../lib/imageSlider";
 import "../../styles/services-page-styles/statisticsService.css";
 import { StatisticsBg1, StatisticsBg2 } from "../../utils/services-utils/images";
-//import formatNumber from "../../lib/formateNumber";
 const StatisitcsService = () => {
-    /*    const statisticsServices = {
-           medicalCenters: 300,
-           professionalDoctors: 200,
-           customers: 5300
-       } */
+
     const images = [
         StatisticsBg1, StatisticsBg2
     ]
@@ -15,25 +10,6 @@ const StatisitcsService = () => {
     return (
         <div className='statistics-service-container hideScrollbar'>
             <ImageSlider images={images} />
-
-            {/*    <div className="statistics-service-box">
-                <p className="statistics-service-count">
-                    {formatNumber(statisticsServices.medicalCenters)}+
-                </p>
-                <p className="statistics-service-text">Medical Centers</p>
-            </div>
-            <div className="statistics-service-box">
-                <p className="statistics-service-count">
-                    {formatNumber(statisticsServices.professionalDoctors)}+
-                </p>
-                <p className="statistics-service-text">Professional Doctors</p>
-            </div>
-            <div className="statistics-service-box">
-                <p className="statistics-service-count">
-                    {formatNumber(statisticsServices.customers)}+
-                </p>
-                <p className="statistics-service-text">Customers</p>
-            </div> */}
         </div>
     )
 }
--- src/lib/imageSlider.tsx
+++ src/lib/imageSlider.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
-import { FcNext, FcPrevious } from "react-icons/fc";
-import "../index.css"
+import "../index.css";
 const ImageSlider = ({ images }: { images: string[] }) => {
     const [isVisible, setIsVisible] = useState(false)
     const [direction, setDirection] = useState("slider-image-right")
@@ -16,11 +15,11 @@ const ImageSlider = ({ images }: { images: string[] }) => {
         visibilityHandler()
         setDirection("slider-image-right")
     };
-    const prevSlide = () => {
+ /*    const prevSlide = () => {
         setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
         visibilityHandler()
         setDirection("slider-image-left")
-    };
+    }; */
 
     useEffect(() => {
         visibilityHandler()
@@ -32,12 +31,12 @@ const ImageSlider = ({ images }: { images: string[] }) => {
     }, [currentIndex])
 
     return (
-        <div className="xl:w-[80%] w-[92%] h-full m-auto flex hideScrollbar" style={{ display: isVisible ? "flex" : "none" }}>
-            <button onClick={prevSlide} className='slider-btn'><FcPrevious /> </button>
+        <div className="w-[100%] h-full m-auto flex hideScrollbar" style={{ display: isVisible ? "flex" : "none" }}>
+            {/*             <button onClick={prevSlide} className='slider-btn'><FcPrevious /> </button> */}
             <div className='w-full flex justify-center items-center'>
                 <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className={direction} />
             </div>
-            <button onClick={nextSlide} className='slider-btn'><FcNext /></button>
+            {/*         <button onClick={nextSlide} className='slider-btn'><FcNext /></button> */}
         </div>
     );
 };
