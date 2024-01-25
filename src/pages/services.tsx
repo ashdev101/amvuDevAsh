2c0eda1d01b2e0bb501068dd1468d51f037f92f0
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-01-25 14:36:58 +0530

    fr

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx"
 import FAService from "../components/services-page-components/FAService"
 import SUFStorage from "../components/services-page-components/SUFStorage"
 import ServicesTop from "../components/services-page-components/ServicesTop"
@@ -5,9 +6,10 @@ import StatisitcsService from "../components/services-page-components/Statisitcs
 import UISDSVControl from "../components/services-page-components/UI-SDS-VControl"
 import "../styles/services-page-styles/services.css"
 import { BottomImage } from "../utils/services-utils/images"
+import { useInView } from "react-intersection-observer"
 
 const ServicesPage = () => {
-
+    const { ref, inView } = useInView()
     return (
         <div className="bg-[#F6F6F6]">
             <ServicesTop />
@@ -24,7 +26,7 @@ const ServicesPage = () => {
             {/* Medical Centers, Professional Doctors, Customers*/}
             <StatisitcsService />
 
-            <div className="services-bottom-div">
+            <div className={clsx("services-bottom-div", inView ? "animate-fadedown" : "animate-fadeout")} ref={ref}>
                 <div className=" absolute 2xl:bottom-[13%] xl:bottom-[10%] lg:bottom-[8%] md:bottom-[6%] bottom-[5%] left-[5%]">
                     <h2 className="services-bottom-div-h2">
                         Ambuvians <br /> <span className="text-[#B7B5B5]">Healthcare</span>
