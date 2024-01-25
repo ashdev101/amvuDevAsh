113f833911012642de53e25653cd055c2027ea5c
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2024-01-25 12:56:41 +0530

    iihekn

--- src/components/services-page-components/components/BYCAmbulance.tsx
+++ src/components/services-page-components/components/BYCAmbulance.tsx
@@ -1,16 +1,19 @@
+import { useInView } from "react-intersection-observer"
 import "../../../styles/services-page-styles/BYCAmbulance.css"
 import { CallUsIconGreen, SmsIconGreen } from "../../../utils/services-utils/images"
+import clsx from "clsx"
 const BYCAmbulance = () => {
+    const { ref, inView } = useInView()
     return (
-        <div className="flex flex-wrap  lg:gap-[25px] sm:gap-[20px] gap-[10px] md:px-[40px] px-[10px] items-center justify-center">
+        <div className="flex flex-wrap  lg:gap-[25px] sm:gap-[20px] gap-[10px] md:px-[40px] px-[10px] items-center justify-center" ref={ref}>
             {/* BLS , PTS div */}
-            <div className="BLS-ALS-div">
+            <div className={clsx("BLS-ALS-div", inView ? "zoom-in" : "animate-fadeout")}>
                 <a href="" className="BYCA-para-link">BLS</a>
                 <a href="" className="BYCA-para-link">PTS</a>
             </div>
 
             {/* Book Your Customised Ambulance div for - Large Devices */}
-            <div className="hidden md:flex flex-col BYC-Ambulance-div">
+            <div className={clsx("hidden md:flex flex-col BYC-Ambulance-div", inView ? "zoom-in" : "animate-fadeout")}>
                 <div className="flex flex-col justify-center items-center w-full mt-[5%]">
                     <h2 className="BYC-Ambulance-h2">Book Your Customised Ambulance</h2>
                     <a href="" className="BYC-Ambulance-link">Book Now</a>
@@ -20,15 +23,15 @@ const BYCAmbulance = () => {
                     <a href="">   <img src={SmsIconGreen} alt="Call Us" className="BYCA-message-icons" /></a>
                 </div>
             </div>
- 
+
             {/* ALS , MTS div */}
-            <div className="BLS-ALS-div">
+            <div className={clsx("BLS-ALS-div", inView ? "zoom-in" : "animate-fadeout")}>
                 <a href="" className="BYCA-para-link">ALS</a>
                 <a href="" className="BYCA-para-link">MTS</a>
             </div>
 
             {/* Book Your Customised Ambulance div for- Small Devices */}
-            <div className="flex flex-col md:hidden BYC-Ambulance-div">
+            <div className={clsx("flex flex-col md:hidden BYC-Ambulance-div", inView ? "zoom-in" : "animate-fadeout")}>
                 <div className="flex flex-col justify-center items-center w-full mt-[5%]">
                     <h2 className="BYC-Ambulance-h2">Book Your Customised Ambulance</h2>
                     <div className="flex w-full justify-evenly items-center  mt-[3%]">
